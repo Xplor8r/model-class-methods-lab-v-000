8b31d687321bfaeb5854d6b891dcc2e43147a9eb
Michael Pieschel <sixtyhectares@yahoo.com>
2018-11-04 23:13:20 +0000

    automatically backed up by learn

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -30,4 +30,9 @@ class Boat < ActiveRecord::Base
   def self.with_three_classifications
     joins(:classifications).group("boats.id").having("COUNT(*) = 3").select("boats.*")
   end
+
+  def 
+    
+  end
+  end
 end
